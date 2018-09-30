5022de7449f6362b48d220d0ebf85da6a55d0b6d
ezcorn <ezcorn@outlook.com>
2018-09-30 10:54:29 +0800

    change format

--- server.go
+++ server.go
@@ -3,12 +3,12 @@ package main
 import "github.com/ezcorn/goe"
 
 /*
-	Try not to manually modify this file
-	You can use the [ got ] tools to build project
+ * 	Try not to manually modify this file
+ * 	You can use the [ got ] tools to build project
 
-	https://github.com/ezcorn/got.git
-	@author ezcorn
-*/
+ * 	https://github.com/ezcorn/got.git
+ * 	@author ezcorn
+ */
 func main() {
 	goe.InitServer()
 }

d2427fed555a5a1f4e43cf1df4a70798c73268ad
ezcorn <ezcorn@outlook.com>
2018-09-30 10:47:32 +0800

    add github address

--- server.go
+++ server.go
@@ -4,6 +4,10 @@ import "github.com/ezcorn/goe"
 
 /*
 	Try not to manually modify this file
+	You can use the [ got ] tools to build project
+
+	https://github.com/ezcorn/got.git
+	@author ezcorn
 */
 func main() {
 	goe.InitServer()
